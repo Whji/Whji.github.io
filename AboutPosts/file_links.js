da8ab6b9c7383e0dd22b773eb8c76cbc5046eecf
Whji <jiwoohyun@kaist.ac.kr>
2025-01-10 19:45:45 +0900

    수정사항 확인용 file_llink추가

--- AboutPosts/file_links.js
+++ AboutPosts/file_links.js
@@ -1,4 +1,8 @@
 export const flinks = [
+    'blog_posting_template_fortest_h.md',
+    'blog_posting_template_fortest_d.md',
+    'blog_posting_template_fortest_cw.md',
+    'blog_posting_template_fortest_as.md',
     'ps_start.md',
     'blog_posting_template.md'
   ];
