30a3427744879dbb38cb0aa94d4175344c22f750
Whji <jiwoohyun@kaist.ac.kr>
2025-03-04 12:53:10 +0900

    coursework의 hello world 테스트 파일 삭제

--- AboutPosts/file_links.js
+++ AboutPosts/file_links.js
@@ -13,6 +13,5 @@ export const flinks = [
     'project_1_github_blog_pt1.md',
     'ps_boj_1978.md',
     'ps_start.md',
-    'ps_start_info.md',
-    'blog_posting_template.md'
+    'ps_start_info.md'
   ];
